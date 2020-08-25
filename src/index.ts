0c4f3a84889d5eed24fc535b68b353c028551d0d
Bojackxiang <batmandocode@gmail.com>
2020-08-25 15:13:54 +1000

    Migrate the data and create first data in postgresql

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,11 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entitlies/Post";
-import path from "path";
-
-const init = {
-  migrations: {
-    path: path.join(__dirname, "/.migrations"),
-    pattern: /^[\w-]+\d+\.[tj]s$/,
-  },
-  entities: [Post],
-  dbName: "lireddit",
-  type: "postgresql",
-  debug: !__prod__,
-} as Parameters<typeof MikroORM.init>[0];
+import init from "./mikro-orm.config";
 
 const main = async () => {
   const orm = await MikroORM.init(init);
-
+  await orm.getMigrator().up();
   const post = orm.em.create(Post, { title: "My first post" });
   await orm.em.persistAndFlush(post);
   // await orm.em.nativeInsert(post);
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,17 @@
+import { Post } from "./entitlies/Post";
+import path from "path";
+import { __prod__ } from "./constants";
+import { MikroORM } from "@mikro-orm/core";
+
+export default {
+  migrations: {
+    path: path.join(__dirname, "/.migrations"),
+    pattern: /^[\w-]+\d+\.ts$/,
+  },
+  entities: [Post],
+  dbName: "alexreddit",
+  type: "postgresql",
+  user:"postgres",
+  password: "911004",
+  debug: !__prod__,
+} as Parameters<typeof MikroORM.init>[0];
